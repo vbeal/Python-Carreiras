c30212d16b411538d37496e87b25d16a1c2623aa
Victor Beal Filho <38515431+vbeal@users.noreply.github.com>
2024-12-05 19:42:55 +0000

    Adicionando listagem de vagas em Endpoint de API

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+vagas = [
+    {
+        'id': 1,
+        'titulo': 'Desenvolvedor Web',
+        'empresa': 'Upwork',
+        'local': 'São Paulo',
+        'salario': 'R$ 2.000,00'
+    },
+    {
+        'id': 2,
+        'titulo': 'Desenvolvedor Python',
+        'empresa': 'Upwork',
+        'local': 'São Paulo',
+        'salario': 'R$ 2.000,00'
+    },
+    {
+        'id': 3,
+        'titulo': 'Desenvolvedor Java',
+        'empresa': 'Upwork',
+        'local': 'São Paulo',
+        'salario': 'R$ 3.000,00'
+    },
+    {
+        'id': 4,
+        'titulo': 'Desenvolvedor C#',
+        'empresa': 'Upwork',
+        'local': 'São Paulo',
+        'salario': 'R$ 1.500,00'
+    },
+    {
+        'id': 5,
+        'titulo': 'Desenvolvedor PHP',
+        'empresa': 'Upwork',
+        'local': 'Paraná',
+        'salario': 'R$ 1.000,00'
+    },
+]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', vagas=vagas)
+
+
+@app.route('/api/vagas')
+def listar_vagas():
+  return jsonify(vagas)
 
 
 if __name__ == '__main__':
